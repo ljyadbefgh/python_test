41eae92e7bb92b6a6532af940e28737ecfc7d5bf
lianjiayi <89287689@qq.com>
2018-12-17 07:29:17 +0800

    第二天培训上午课开始前，解决tensorflow安装后无法使用问题，并修改代码解决在测试案例中CPU不支持问题

--- testTensorflow.py
+++ testTensorflow.py
@@ -1,3 +1,8 @@
+#成功解决本电脑出现的：Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX AVX2
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#成功解决本电脑出现的：Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX AVX2
+#测试tensorflow是否安装好
 import tensorflow as tf
 a = tf.constant('hello world!')
 sess = tf.Session()
